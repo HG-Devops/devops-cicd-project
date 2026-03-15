10d0543eb0ebb15748ea44e80c9a7f2b2c7805ce
Hemanth <hemanthgowtham.devops@gmail.com>
2026-03-15 18:58:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install nginx -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              echo "<h1>CI/CD Deployed Website</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "DevOps-CICD-Server"
+  }
+}
